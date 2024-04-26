fed6906509850a05a11e76ce712dc25b1dafdca1
manu <manu.softwareengineer@gmail.com>
2024-04-26 17:24:45 +0000

    disregarded the check endpoint

--- api/api.py
+++ api/api.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI,Form,Response,Request
+from fastapi import FastAPI
 from pydantic import BaseModel
 from chatbot import get_response
-from model import model 
-import pandas as pd
 from dotenv import load_dotenv
 import os
 from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
+
 app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
@@ -23,48 +22,6 @@ class Message(BaseModel):
     session_id: str
 
 port = int(os.getenv("PORT", 9000))
-
-@app.post('/check')
-async def check_health_data(
-    response: Response,
-    age: int = Form(...),
-    sex: int = Form(...),
-    cp: int = Form(...),
-    trestbps: int = Form(...),
-    chol: int = Form(...),
-    fbs: int = Form(...),
-    restecg: int = Form(...),
-    thalach: int = Form(...),
-    exang: int = Form(...),
-    oldpeak: float = Form(...),
-    slope: int = Form(...),
-    ca: int = Form(...),
-    thal: int = Form(...)
-):
-    new_data = pd.DataFrame({
-        "age": [age],
-        "sex": [sex],
-        "cp": [cp],
-        "trestbps": [trestbps],
-        "chol": [chol],
-        "fbs": [fbs],
-        "restecg": [restecg],
-        "thalach": [thalach],
-        "exang": [exang],
-        "oldpeak": [oldpeak],
-        "slope": [slope],
-        "ca": [ca],
-        "thal": [thal],
-    }, index=[0])
-
-    answer = model.predict(new_data)
-    if answer == 1:
-        response_text = "Status: You have heart disease"
-    elif answer == 0:
-        response_text = "Status: You not have heart disease"
-    
-    response.set_cookie(key="health_status", value=response_text, secure=False)
-    return {"response": response_text}
 
 
 @app.post("/chat")
