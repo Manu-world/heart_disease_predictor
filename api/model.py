ca9ed2176805b21efe4f678a6db839ad005de8f1
manu <manu.softwareengineer@gmail.com>
2024-04-27 16:55:22 +0000

    connected to mongodb

--- api/model.py
+++ api/model.py
@@ -1,15 +1,15 @@
-# import requests
+import requests
 
-# url = "https://heart-disease-predictor-7.onrender.com/chat"
-# headers = {"Content-Type": "application/json"}
-# data = {
-#      "text": "What are the symptoms of heart disease?",
-#      "context": "The patient is a 57-year-old male with a history of smoking.",
-#      "session_id": "12345"
-# }
+url = "https://heart-disease-predictor-8.onrender.com/chat"
+headers = {"Content-Type": "application/json"}
+data = {
+     "text": "What are the symptoms of heart disease?",
+     "context": "The patient is a 57-year-old male with a history of smoking.",
+     "session_id": "12345"
+}
 
-# response = requests.post(url, json=data, headers=headers)
-# print(response.json())
+response = requests.post(url, json=data, headers=headers)
+print(response.json())
 
 # @app.post('/check')
 # async def check_health_data(
